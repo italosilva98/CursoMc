db06c5797be2b095d4e5b057b147962e7eb54cbb
italosilva98 <italosilvestre3@icloud.com>
2020-09-21 19:15:21 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/silvaitalo/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.silvaitalo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.silvaitalo.cursomc.domain.Categoria;
+import com.silvaitalo.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired // Automaticamente instanciada pelo spring
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);
+	}
+
+}
